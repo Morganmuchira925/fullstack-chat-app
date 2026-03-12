5c08d79783d0fc331fad7f18e47d631021a2f89c
Morganmuchira925 <morganisrael2005@gmail.com>
2026-03-12 18:57:16 +0300

    Added AppName to ChatBox

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,9 +17,9 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Welcome to Convobuzz⚆_⚆</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Select a chat to start messaging or explore the settings to customize your experience.
         </p>
       </div>
     </div>
